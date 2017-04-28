29c07c94810c45fb9c93bcc898ce625ac72f4115
Pantelis Monogioudis <monogioudis@yahoo.com>
2017-04-28 15:57:36 -0400

    implemented NVIDIA network

--- training.py
+++ training.py
@@ -6,10 +6,11 @@ from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda
 from keras.layers.convolutional import Conv2D
 from keras.layers.pooling import MaxPool2D
+from keras.layers.normalization import BatchNormalization
 
 # driving the car forward in track 1
-root_data_dir = '../datasets/'
-drive_dirs = ['udacity-data-track1/']
+root_data_dir = '../datasets/udacity-car-sim-data/'
+drive_dirs = ['final-forward-track1/']
 #drive_dirs = ['reverse-track1/']#, 'forward-track2/', 'reverse-track2/']#, ]
 
 images = []
@@ -21,23 +22,28 @@ for directory in drive_dirs:
     with open(os.path.join(root_data_dir, directory, 'driving_log.csv')) as csvfile:
         reader = csv.reader(csvfile)
         for line in reader:
-            # use the center image as a feature (index 0)
-            # the following line uses separator for simulator data collected in windows
-            filename = line[0].split('\\')[-1]
-            #image = cv2.imread(os.path.join(root_data_dir, directory, 'IMG/', filename))
-            image = cv2.imread(os.path.join(root_data_dir, directory, filename))
-            #print(os.path.join(root_data_dir, directory, filename))
-            images.append(image)
-            measurements.append(float(line[3]))
+            # use all available cameras
+            for camera in range(3):
+                # the following line uses separator for simulator data collected in windows
+                # filename = line[0].split('\\')[-1]
+                # the following line uses separator for simulator data collected in OSX
+                filename = line[camera].split('/')[-1]
+                image = cv2.imread(os.path.join(root_data_dir, directory, 'IMG/', filename))
+                #image = cv2.imread(os.path.join(root_data_dir, directory, filename))
+                #print(os.path.join(root_data_dir, directory, filename))
+                images.append(image)
+                measurements.append(float(line[3]))
 
 # augmentation
 for image, measurement in zip(images, measurements):
     augmented_images.append(image)
-    augmented_images.append(cv2.flip(image,1))
+    augmented_images.append(cv2.flip(image, 1))
     augmented_measurements.append(measurement)
     augmented_measurements.append(measurement*(-1.0))
 
-print('Number of images:', len(images))
+print('Number of original images:', len(images))
+print('Number of augmented images:', len(augmented_images))
+
 
 # for img in images:
 #     cv2.imshow('image', img)
@@ -47,13 +53,40 @@ print('Number of images:', len(images))
 X_train = np.array(augmented_images)
 Y_train = np.array(augmented_measurements)
 
-# Simple network
-model = Sequential()
-model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
-model.add(Flatten())
-model.add(Dense(1))
+
+def simple_model():
+
+    model = Sequential()
+    model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
+    model.add(Flatten())
+    model.add(Dense(1))
+    return model
+
+
+def nvidia_model():
+
+    model = Sequential()
+
+    model.add(BatchNormalization(epsilon=0.001, axis=3, input_shape=(160, 320, 3)))
+
+    model.add(Conv2D(24, 5, 5, border_mode='valid', activation='relu', subsample=(2, 2)))
+    model.add(Conv2D(36, 5, 5, border_mode='valid', activation='relu', subsample=(2, 2)))
+    model.add(Conv2D(48, 5, 5, border_mode='valid', activation='relu', subsample=(2, 2)))
+    model.add(Conv2D(64, 3, 3, border_mode='valid', activation='relu', subsample=(1, 1)))
+    model.add(Conv2D(64, 3, 3, border_mode='valid', activation='relu', subsample=(1, 1)))
+    model.add(Flatten())
+    model.add(Dense(1164, activation='relu'))
+    model.add(Dense(100, activation='relu'))
+    model.add(Dense(50, activation='relu'))
+    model.add(Dense(10, activation='relu'))
+    model.add(Dense(1, activation='tanh'))
+
+    return model
+
+#model = simple_model()
+model = nvidia_model()
+
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, Y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
+model.fit(X_train, Y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
 
 model.save('model.h5')
-
